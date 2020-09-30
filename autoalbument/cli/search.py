e08e41caf910357360c44821988ee2e74a8485d0
Alex Parinov <creafz@gmail.com>
2020-09-30 11:29:12 +0300

    Add structured config schema

--- autoalbument/cli/search.py
+++ autoalbument/cli/search.py
@@ -2,12 +2,16 @@ import os
 import shutil
 
 import hydra
+from hydra.core.config_store import ConfigStore
 from omegaconf import OmegaConf
 
+from autoalbument.config.faster_autoaugment import FasterAutoAugmentSearchConfig
 from autoalbument.faster_autoaugment.search import FasterAutoAugment
 from autoalbument.utils.hydra import get_hydra_config_dir, get_dataset_filepath
 
 OmegaConf.register_resolver("config_dir", get_hydra_config_dir)
+cs = ConfigStore.instance()
+cs.store(name="search", node=FasterAutoAugmentSearchConfig)
 
 
 @hydra.main(config_name="search")
---
+++ autoalbument/config/faster_autoaugment.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from omegaconf import MISSING
+
+
+@dataclass
+class PreprocessingConfig:
+    mean: List[float] = field(default_factory=lambda: [0.485, 0.456, 0.406])
+    std: List[float] = field(default_factory=lambda: [0.229, 0.224, 0.225])
+
+
+@dataclass
+class DataloaderConfig:
+    batch_size: int = 128
+    shuffle: bool = True
+    num_workers: int = 4
+
+
+@dataclass
+class DataConfig:
+    preprocessing: PreprocessingConfig
+    dataloader: DataloaderConfig
+    dataset_file: str = 'dataset.py'
+    input_dtype: str = 'uint8'
+
+
+@dataclass
+class OptimConfig:
+    epochs: int = 20
+    main_lr: float = 1e-3
+    policy_lr: float = 1e-3
+
+
+@dataclass
+class ModelConfig:
+    num_classes: int = MISSING
+    architecture: str = 'resnet18'
+    pretrained: bool = False
+    cls_factor: float = 0.1
+    gp_factor: float = 10
+    temperature: float = 0.05
+    num_sub_policies: int = 150
+    num_chunks: int = 8
+    operation_count: int = 4
+
+
+@dataclass
+class FasterAutoAugmentSearchConfig:
+    model: ModelConfig
+    data: DataConfig
+    optim: OptimConfig
+    device: str = 'cuda'
