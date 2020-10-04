b8d0236ab138e8f2cd74bff44c03b535f83e0eeb
Alex Parinov <creafz@gmail.com>
2020-10-04 17:17:08 +0300

    Remove kornia from dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
         "albumentations>=0.4.6",
         "torch>=1.6.0",
         "hydra-core>=1.0",
-        "kornia>=0.4.0",
         "timm",
         "tqdm",
     ],
@@ -17,6 +16,7 @@ setup(
             "autoalbument-search = autoalbument.cli.search:main",
         ],
     },
+    extras_require={"tests": ["pytest"]},
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
 )
