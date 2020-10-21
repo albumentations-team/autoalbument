5dccb0666435420c2461509b3a22f5f072e46be6
Alex Parinov <creafz@gmail.com>
2020-10-21 18:24:42 +0300

    Use file copying fallback on Windows

--- autoalbument/utils/files.py
+++ autoalbument/utils/files.py
@@ -1,8 +1,13 @@
+import shutil
 import tempfile
+import platform
 import os
 
 
 def symlink(src, dst):
-    tmp_dst = tempfile.mktemp(dir=dst.parents[0])
-    os.symlink(src, tmp_dst)
-    os.replace(tmp_dst, dst)
+    if platform.system == "Windows":
+        shutil.copy2(src, dst)
+    else:
+        tmp_dst = tempfile.mktemp(dir=dst.parents[0])
+        os.symlink(src, tmp_dst)
+        os.replace(tmp_dst, dst)
