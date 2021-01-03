438d1fc1186f5f3509632692b716c488a74358e7
Alex Parinov <creafz@gmail.com>
2021-01-03 15:35:07 +0400

    Fix function name

--- autoalbument/faster_autoaugment/albumentations_pytorch/functional.py
+++ autoalbument/faster_autoaugment/albumentations_pytorch/functional.py
@@ -84,8 +84,10 @@ def random_crop_and_pad(img_batch, crop_size):
     y2 = y1 + crop_size
     x1 = ((width - crop_size) * w_start).type(torch.IntTensor)
     x2 = x1 + crop_size
-    pad_top_bottom = (height - crop_size) // 2
-    pad_left_right = (width - crop_size) // 2
+    pad_bottom = (height - crop_size) // 2
+    pad_top = height - crop_size - pad_bottom
+    pad_right = (width - crop_size) // 2
+    pad_left = width - crop_size - pad_right
     img_batch = img_batch[..., y1:y2, x1:x2]
-    img_batch = F.pad(img_batch, (pad_left_right, pad_left_right, pad_top_bottom, pad_top_bottom), "constant", 0)
+    img_batch = F.pad(img_batch, (pad_left, pad_right, pad_top, pad_bottom), "constant", 0)
     return img_batch
