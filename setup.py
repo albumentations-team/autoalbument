3db40842122f3c24306da99903feb7d085e9814f
Michael Monashev <mmb@ya.ru>
2021-09-04 17:42:08 +0300

    Update requipments
    Сейчас с библиотекой ставятся очень старые версии timm и segmentation-models-pytorch. Не уверен, что нужно фиксировать версию timm, ибо либа довольно стабильная и если что, segmentation-models-pytorch поставит нужную версию. Сейчас версия timm такая же, как в https://github.com/qubvel/segmentation_models.pytorch/blob/master/requirements.txt .

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
         "pytorch-lightning>=1.1.8,<1.2",
         "torch>=1.6.0",
         "hydra-core>=1.0",
-        "timm==0.3.2",  # This version is required for segmentation-models-pytorch
-        "segmentation-models-pytorch>=0.1.3",
+        "timm>=0.4.12",  # This version is required for segmentation-models-pytorch
+        "segmentation-models-pytorch>=0.2.0",
         "tqdm",
         "click",
         "colorama",
